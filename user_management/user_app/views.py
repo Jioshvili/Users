c0fe1093aa7711a1157143a11127eac1577633ab
Mariam Jioshvili <128077918+Jioshvili@users.noreply.github.com>
2023-12-16 23:47:25 +0400

    Update views.py

--- user_management/user_app/views.py
+++ user_management/user_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_app/user_list.html', {'users': users})
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = UserForm()
+    return render(request, 'user_app/create_user.html', {'form': form})
