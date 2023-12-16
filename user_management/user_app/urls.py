1142f1104cf10de2a4cbea599aa08c4094de4f5f
Mariam Jioshvili <128077918+Jioshvili@users.noreply.github.com>
2023-12-16 23:57:00 +0400

    Update urls.py

--- user_management/user_app/urls.py
+++ user_management/user_app/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import user_list, create_user
+
+urlpatterns = [
+    path('user_list/', user_list, name='user_list'),
+    path('create_user/', create_user, name='create_user'),
+]
 
